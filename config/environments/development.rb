c5359455b46e9045839d249fc30d7af07adce12b
Patrick Lo <patricklo87@gmail.com>
2014-10-03 15:57:33 -0700

    update development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-demo_app::Application.configure do
+Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
